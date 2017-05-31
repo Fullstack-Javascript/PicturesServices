942051451cef1a0d12f1e0103769333422ab4463
Azad Ramasamy <Azad Ramasamy>
2017-05-30 22:05:51 -0400

    Experimenting Dropbox API calls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var mdb = require('./config/db');
 var nodeServer = require('./config/server');
 var MongoClient = require('mongodb').MongoClient;
+var Dropbox = require('dropbox-node');
 
 nodeServer.start();
 
@@ -19,9 +20,41 @@ var validateLogin = exports.validateLogin = function (db,req, res) {
 
 MongoClient.connect(mdb.url, function(err, db){
     nodeServer.app.get('/validateLogin', function(req, res){
-        validateLogin(db,req,res);     
+        connectDropbox();
+        //validateLogin(db,req,res);     
     });
     nodeServer.app.use(function(req, res){
         res.sendStatus(404); 
     });
 });
+
+var connectDropbox = exports.connectDropbox = function () {
+    var dropbox = new Dropbox.DropboxClient('cfob4wge1sykhvh', 'rabair1lb8w7144');
+   // var dropbox = new DropboxClient("cfob4wge1sykhvh", "rabair1lb8w7144", "Ogrp3bYPHeAAAAAAAAAAGEc77phgiZ415czsEpZF1dvafrRhDrpRgCwNWTCaGHL4", access_token_secret)
+    console.log("dropbox: " + dropbox);
+
+    /*dropbox.getAccountInfo(function (err, data) {
+        if (err) console.log('Error: ' + err)
+        else console.log(data.display_name + ', ' + data.email)
+    })*/
+
+    dropbox.getAccessToken('azadramasamy@gmail.com', 'Chandra@88', function (err, token, secret) {
+    // Upload foo.txt to the Dropbox root directory.
+    console.log("token: " + token);
+    console.log("secret: " + secret);
+    dropbox.putFile('foo.txt', 'foo.txt', function (err, data) {
+        console.log("data: "+data)
+        if (err) return console.error(err)
+
+        // Move it into the Public directory.
+        /*dropbox.move('foo.txt', 'Public/foo.txt', function (err, data) {
+            if (err) return console.error(err)
+
+            // Delete the file.
+            dropbox.deleteItem('Public/foo.txt', function (err, data) {
+                if (err) console.error(err.stack)
+            })
+        })*/
+    })
+    })
+}
